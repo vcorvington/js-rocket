965a042d03f5ea30044c1e842ed0f2f60c69d4d1
vcorvington <vcorvington@gmail.com>
2017-03-19 21:03:54 -0400

    Finalized rocket

--- js/rocket.js
+++ js/rocket.js
@@ -7,11 +7,27 @@ var changeState = function (state) {
 	countdownNumber = 10;
 	document.getElementById("countdown").innerHTML = countdownNumber;
 
-
+//countdown
 if (state == 2) {
 		timer = setInterval(function() {
 			countdownNumber = countdownNumber-1;
 			document.getElementById("countdown").innerHTML = countdownNumber; 
+			
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				// be nervous
+				document.getElementById('nervous').className = 'nervous show';
+			} else {document.getElementById('nervous').className = 'nervous';
+
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				// can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else { 
+				document.getElementById('cant-wait').className = 'cant-wait';
+
+			}	
+
 			if (countdownNumber <=0) {	
 				changeState(3);
 			};
@@ -25,7 +41,7 @@ if (state == 2) {
 			console.log('randomNumber:', randomNumber)
 
 			// success
-			if (randomNumber > 2) {
+			if (randomNumber > 5) {
 				changeState(4);
 
 			} else {
